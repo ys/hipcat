3e46d474612459db46abb0a8dba2c93b28aeca3e
Yannick <yannick.schutz@gmail.com>
2015-11-25 16:38:45 +0100

    Update a bit UI and components

--- javascript/entry.js
+++ javascript/entry.js
@@ -7,57 +7,136 @@ require('dotenv').load()
 import React from "react"
 import ReactDOM from "react-dom"
 
-import request from "request"
-
-class UserLogin extends React.Component {
+class ChatMainWindow extends React.Component {
   constructor(props, context) {
     super(props, context)
+
+    this.state = {
+      apiToken: localStorage.getItem('apiToken')
+    }
+
+    this.saveToken = this.saveToken.bind(this)
+    this.signOut = this.signOut.bind(this)
+  }
+
+  saveToken(token) {
+    localStorage.setItem('apiToken', token)
+    this.setState({apiToken: token})
+  }
+
+  signOut() {
+    localStorage.removeItem('apiToken')
+    this.setState({apiToken: undefined})
+  }
+
+  render() {
+    if (this.state.apiToken) {
+      return <div>
+        <SignOut onSignOutClick={this.signOut}/>
+        <Chat apiToken={this.state.apiToken}/>
+			</div>
+		} else {
+      return <LoginForm onTokenSubmit={this.saveToken}/>
+		}
+	}
+}
+
+class Chat extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+  }
+  render() {
+    return <table className="table">
+    </table>
+  }
+}
+
+class SignOut extends React.Component {
+  render() {
+    return <a href='#' onClick={this.props.onSignOutClick}>Sign out</a>
+  }
+}
+
+class LoginForm extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      error: false
+    }
+
     this.handleSubmit = this.handleSubmit.bind(this)
     this.checkToken = this.checkToken.bind(this)
   }
 
   render() {
-		return <form onSubmit={this.handleSubmit}>
-      <label htmlFor="apiToken">Hipchat Token</label>
-      <input type="password" id="apiToken" placeholder="Token"/>
-			<button type="submit">Sign in</button>
+    var errorDiv
+    console.log(this.state)
+    if (this.state.error) {
+      errorDiv = <div className="alert alert-danger" >Your token is invalid</div>
+    }
+		return <form className="form" onSubmit={this.handleSubmit}>
+      <div className="form-group">
+        {errorDiv}
+        <label htmlFor="apiToken">Hipchat Token</label>
+        <input className="form-control" type="password" id="apiToken" placeholder="Token"/>
+      </div>
+			<button className="btn btn-default" type="submit">Sign in</button>
 		</form>
   }
 
   handleSubmit(e) {
     e.preventDefault()
     var apiToken = e.target.apiToken.value
-    if (this.checkToken(apiToken)) {
-      localStorage.setItem('apiToken', apiToken)
-    }
+    this.checkToken(apiToken)
+    .then((body) => {
+      this.props.onTokenSubmit(apiToken)
+    })
+    .catch( (error) => {
+      this.setState({error: true})
+      console.log("something horrible happenned", error)
+    })
   }
 
   checkToken(token) {
-    new Promise(function (fulfill, reject){
-      var url = process.env.HIPCHAT_HOST + "/v2/user"
-      console.log(url)
-      request.get(url, {headers: { "Authorization": "Bearer "+ token}}, function (error, response, body) {
-        if (error) {
-          return reject(error)
-        }
-        console.log(body)
-        fulfill(body)
-      })
-    })
+    var client = new Hipchat(token)
+    return client.users()
   }
 }
 
 class Hipchat {
   constructor(token) {
-    this.options = {
-      hostname: process.env.HIPCHAT_HOST,
-      port: 443,
-      headers: {
-        "Authorization": "Bearer " + token
-      }
-    }
+		this.url = "https://" + process.env.HIPCHAT_HOST
+		this.defaultHeaders = { "Authorization": "Bearer " + token }
+    this.users = this.users.bind(this)
+    this.get = this.get.bind(this)
   }
 
+
+  users() {
+    return this.get("/v2/user")
+	}
+
+  get(path, qs) {
+    return fetch(this.url + path, {headers: this.defaultHeaders })
+    .then(function(res) {
+      if (!res.ok) {
+        return res.json().then(function(json) { throw json })
+			}
+      return res.json()
+    })
+    .then(function(json) { return json })
+	}
+
+  post(path, body) {
+    return fetch(this.url + path, { body: body, method: "post", headers: this.defaultHeaders })
+    .then(function(res) {
+      if (!res.ok) {
+        return res.json().then(function(json) { throw json })
+			}
+      return res.json()
+    })
+    .then(function(json) { return json })
+	}
 }
 
-ReactDOM.render(<UserLogin />, document.getElementById('content'))
+ReactDOM.render(<ChatMainWindow />, document.getElementById('content'))
